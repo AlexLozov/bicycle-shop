25faa0b12173cfc3fbfa7c02050aa59f9d54530c
Alexandr Lozov <sasalozov2004@gmail.com>
2025-10-06 18:35:26 +0300

    create a model

---
+++ BicycleShop/src/main/java/com/BicycleShop/model/enteties/Bicycle.java
@@ -0,0 +1,45 @@
+package com.BicycleShop.model.enteties;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "bicycles")
+@Getter
+@Setter
+public class Bicycle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer id;
+
+    @Column(nullable = false)
+    public String brand;
+
+    @Column(nullable = false)
+    public String type;
+
+    @Column(nullable = false)
+    public Float price;
+
+    @Column(nullable = false)
+    public Integer stock;
+
+    @Column(nullable = false)
+    public String description;
+
+    @Column(name = "image_url", nullable = false)
+    public String imageUrl;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    public LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "updated_at", nullable = false)
+    public LocalDateTime updatedAt = LocalDateTime.now();
+
+
+}
